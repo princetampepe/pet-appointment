6d3f876010472cfb5664829c8fdd4a67f047e00b
princetampepe <princechristian.tampepe@ctu.edu.ph>
2026-03-18 15:41:33 +0800

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, '..', 'database.sqlite'));
+const IS_VERCEL = process.env.VERCEL === '1';
+const dbPath = IS_VERCEL
+    ? '/tmp/database.sqlite'
+    : path.join(__dirname, '..', 'database.sqlite');
+
+const db = new Database(dbPath);
 
 // Enable foreign keys
 db.pragma('foreign_keys = ON');
